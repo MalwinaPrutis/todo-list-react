5f0c06bef52c54f55ed1fe36532918b2b8551508
Malwina Prutis <prutis@vp.pl>
2024-11-14 19:31:26 +0100

    Rozwiązane konflikty po pull

---
+++ Downloads/todo-list-react-master (3)/todo-list-react-master/src/App.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import Form from "./Form";
+import Tasks from "./Tasks";
+import Buttons from "./Buttons";
+import Section from "./Section";
+import Header from "./Header";
+import Container from "./Container";
+
+const defaultTasks = [
+  { id: 1, content: "Masaż relaksacyjny", completed: false },
+  { id: 2, content: "Wycieczka rowerowa", completed: true },
+];
+
+function App() {
+  const [hideCompletedTasks, setHideCompletedTasks] = useState(false);
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || defaultTasks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const toggleHideCompleted = () => {
+    setHideCompletedTasks((hideCompletedTasks) => !hideCompletedTasks);
+  };
+
+  const removeTask = (id) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleTaskCompleted = (id) => {
+    setTasks((tasks) =>
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const setAllCompleted = () => {
+    setTasks((tasks) => tasks.map((task) => ({ ...task, completed: true })));
+  };
+
+  const addNewTask = (newTaskContent) => {
+    setTasks((tasks) => [
+      ...tasks,
+      {
+        content: newTaskContent,
+        completed: false,
+        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+      },
+    ]);
+  };
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+      <Section
+        title="Lista zadań"
+        body={
+          <Tasks
+            tasks={tasks}
+            hideCompletedTasks={hideCompletedTasks}
+            removeTask={removeTask}
+            toggleTaskCompleted={toggleTaskCompleted}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideCompletedTasks={hideCompletedTasks}
+            toggleHideCompleted={toggleHideCompleted}
+            setAllCompleted={setAllCompleted}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default App;
---
+++ Downloads/todo-list-react-master (3)/todo-list-react-master/src/tasks.js
@@ -0,0 +1,7 @@
+// src/tasks.js
+
+export const tasks = [
+     { id: 1, content: "Masaż relaksacyjny", completed: false },
+     { id: 2, content: "Wycieczka rowerowa", completed: true },
+   ];
+   
