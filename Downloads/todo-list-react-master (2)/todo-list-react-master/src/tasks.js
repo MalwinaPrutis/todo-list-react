4bb109df9ae541bd879a1c5e849c149a0deb7a16
Malwina Prutis <prutis@vp.pl>
2024-11-18 17:08:34 +0100

    Delete Downloads/todo-list-react-master (2)/todo-list-react-master directory

--- Downloads/todo-list-react-master (2)/todo-list-react-master/src/tasks.js
+++
@@ -1,7 +0,0 @@
-// src/tasks.js
-
-export const tasks = [
-     { id: 1, content: "Masaż relaksacyjny", completed: false },
-     { id: 2, content: "Wycieczka rowerowa", completed: true },
-   ];
-   
